389a8b0ae5862f252d0123d2594127b3601b19d2
beomsooo.kim <lightjd@naver.com>
2024-05-29 16:13:03 +0900

    Added getSum and getSumTest

--- Sample-Test1/cal_test.cpp
+++ Sample-Test1/cal_test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
+#include "../project1/cal.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, getSumTest) {
+	EXPECT_EQ(getSum(5, 10), 15);
+	EXPECT_TRUE(true);
 }
